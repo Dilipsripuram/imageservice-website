cb5ad37459933b679d71a6bcc6255e63c07b7388
Dilip <dilipsripuram@gmail.com>
2025-12-21 17:03:07 -0800

    ignoreing build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useParams, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useParams, useNavigate } from 'react-router-dom';
 import FileManager from './components/FileManager';
 import './assets/css/main.css';
 
